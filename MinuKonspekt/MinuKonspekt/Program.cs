e2268ba5d3bb4e75dba840033e66555fbf9ccf55
Opilane TTHK <opilane@tthk.ee>
2025-09-24 11:46:48 +0300

    värvi

--- MinuKonspekt/MinuKonspekt/Program.cs
+++ MinuKonspekt/MinuKonspekt/Program.cs
@@ -154,6 +154,27 @@ namespace MinuKonspekt
             {
                 Console.WriteLine("Parool on vale,proovi uuesti"); 
             }
+
+            //5 värvituvastus 
+            Console.WriteLine("Milline värv sulle kõige rohkem meeldib?:");
+            string favouriteColour = Console.ReadLine(); 
+            if(favouriteColour == "punane")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+            if (favouriteColour == "oranz")
+            {
+                Console.WriteLine("Kahjuks oranzi ei ole");
+            }
+            if (favouriteColour == "kollane") ;
+            {
+                Console.BackgroudColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.WriteLine("Värvi ei tunne");
+            }
+            Console.WriteLine("Värv muudetud!");
          
         } 
     } 
