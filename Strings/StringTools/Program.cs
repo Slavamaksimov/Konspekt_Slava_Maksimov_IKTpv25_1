4e29d5c04913c4496ad61d5d5620aff4f57ca489
Opilane TTHK <opilane@tthk.ee>
2025-12-19 13:33:09 +0200

    sõnatööriistad /tools of the string

---
+++ Strings/StringTools/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace StringTools
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lause = "tÄna hommikul jõin äras terve ploki moNSTerit, aga üles ei ärganud, tahan veel magada";
+            //Console.WriteLine(lause.Length);
+            //Console.WriteLine(lause);
+            //Console.WriteLine(lause.ToUpper());
+            //Console.WriteLine(lause.ToLower());
+            //Console.WriteLine("Kas lause on sõna \"monsterit\"?:"+lause.ToLower().Contains("monstrit"));
+
+            //List<string> sõnenimikiri = new List<string>() { "auto", "kiirabi", "tuletõrje" };
+            //string kogunimikiri = "";
+            //foreach (var sõna in sõnenimikiri)
+            //{
+            //    kogunimikiri += (" "+sõna.ToUpper()+","); 
+            //}
+            //Console.WriteLine(kogunimikiri);
+
+            //string eesnimi = "Julius";
+            //string perekonnanimi = "Caesar";
+            //string amet = "Noahoidik";
+            //string kuulusKeiser = string.Concat(eesnimi, perekonnanimi, amet);
+            //Console.WriteLine(kuulusKeiser);
+
+            //string kuulusKeiser2 = $"{eesnimi} {perekonnanimi} oli maailma kuulsaim {amet}";
+            //string kuulusKeiser3 = "" + eesnimi + " " + perekonnanimi + " oli maailm kuulsaim " + amet;
+            //Console.WriteLine(kuulusKeiser3);
+            //Console.WriteLine(kuulusKeiser2);
+            //string[] kõiksõnadlauses = lause.Split(' '); 
+            //foreach (var sõnainlause in kõiksõnadlauses)
+            //{
+            //    Console.WriteLine(sõnainlause);
+            //}
+            //string üksSõna = "mustalehmatagavalgelehmasaba";
+            //Console.WriteLine(üksSõna[13]);
+            //Console.WriteLine(üksSõna.IndexOf("l"));
+
+            //Console.WriteLine(" \" ");
+            //Console.WriteLine("a \n b");
+            //Console.WriteLine("AAAA\tBBBB");
+
+        }
+    }
+}
